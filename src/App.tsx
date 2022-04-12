a9e5396a517d4c37930867d2cf3170c24b621a2b
Fülöp Kovács <kovacs.fulop@gmail.com>
2022-04-12 22:45:45 +0200

    Fix the annoying iOS Safari scroll bouncing effect

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import PageWithBreakpoints from "./ui-components/PageWithBreakpoints";
 function App() {
   return (
     <div>
-      <header className="bg-green-500 text-sm text-white text-center py-1">
+      <header className="bg-green-500 text-sm text-white text-center py-1 fixed top-0 left-0 right-0 z-10">
         CogniBot
       </header>
       <ExperimentConfigContextProvider>
