d318324388971790dcd9ea0b6b43fa15fe73f08d
Fülöp Kovács <kovacs.fulop@gmail.com>
2022-04-13 21:13:30 +0200

    Fix the position of the circle shape in the Circle component

--- src/ui-components/chat/Circle.tsx
+++ src/ui-components/chat/Circle.tsx
@@ -6,7 +6,7 @@ const Circle: React.FC<{ fillColor: string }> = ({ fillColor }) => {
       viewBox="0 0 30 30"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <circle cx="30" cy="30" r="15" fill={fillColor} />
+      <circle cx="15" cy="15" r="15" fill={fillColor} />
     </svg>
   );
 };
