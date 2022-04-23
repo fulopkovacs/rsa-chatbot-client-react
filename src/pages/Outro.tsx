f25dd4fdb42b0b20b38378441a9dedaf45964ff4
Fülöp Kovács <kovacs.fulop@gmail.com>
2022-04-23 18:44:50 +0200

    Redirect to `/entry` when the config is missing in Outro.tsx

--- src/pages/Outro.tsx
+++ src/pages/Outro.tsx
@@ -1,14 +1,22 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { ExperimentConfigContext } from "../ExperimentConfigContext";
 import PageTitle from "../ui-components/PageTitle";
 
 function Outro() {
   const contextValue = useContext(ExperimentConfigContext);
+  const navigate = useNavigate();
 
   const outroData = contextValue?.experimentConfig?.outro || {
     title: "",
     body: "",
   };
+
+  useEffect(() => {
+    // TODO: Find a better way to redirect to the `/entry` page when
+    // the config is missing
+    if (!contextValue?.experimentConfig?.outro) navigate("/entry");
+  });
 
   const { title, body } = outroData;
 
