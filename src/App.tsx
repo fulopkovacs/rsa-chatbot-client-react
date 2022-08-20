dc447a6413fc6a80083ade21683525697b768b9e
Fülöp Kovács <kovacs.fulop@gmail.com>
2022-08-20 11:18:31 +0200

    [drop] Check if GH workflow works properly

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { ExperimentConfigContextProvider } from "./ExperimentConfigContext";
 import PageWithBreakpoints from "./ui-components/PageWithBreakpoints";
 
 function App() {
+  console.log("App started");
   return (
     <div>
       <header className="bg-green-500 text-sm text-white text-center py-1 fixed top-0 left-0 right-0 z-10 pointer-events-none">
